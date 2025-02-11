843f5a2fdbb9efde48f9be1731131308fd4154ae
GGallery <antonio@ggallery.it>
2025-02-11 15:10:32 +0100

    feat: create and settings for new command Generate Api Token

---
+++ app/Console/Commands/GenerateApiToken.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\ApiToken;
+use Illuminate\Support\Str;
+
+
+class GenerateApiToken extends Command
+{
+    protected $signature = 'generate:api-token {platform_name}';
+    protected $description = 'Generate an API token for a platform';
+
+    public function handle()
+    {
+        $platformName = $this->argument('platform_name');
+        $token = Str::random(60);
+
+        ApiToken::create([
+            'platform_name' => $platformName,
+            'token' => hash('sha256', $token),
+        ]);
+
+        $this->info("API token for {$platformName}: {$token}");
+    }
+}
