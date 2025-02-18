d6a82ef945d57f28679850c08457d03aba7a3e94
GGallery <antonio@ggallery.it>
2025-02-18 16:56:46 +0100

    fix: new contacts table, now contact seeder is obsolete

--- database/migrations/2025_02_10_154010_create_contacts_table.php
+++ database/migrations/2025_02_10_154010_create_contacts_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->text('message')->nullable();            
+            $table->string('password');
             $table->timestamps();
         });
     }
