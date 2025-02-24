2e9e6a0edfb26056d8b902801091bef891f5d6cd
GGallery <antonio@ggallery.it>
2025-02-24 12:55:51 +0100

    feat: add error logging also to ContactController

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ContactResource;
+use App\Models\ApiTokenPrefix;
 use App\Models\Contact;
+use App\Models\SystemLog;
+use Exception;
 use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\Validator;
 
@@ -25,8 +28,55 @@ class ContactController extends Controller
      */
     public function store(Request $request)
     {
-        $contact = Contact::create($request->all());
-        return new ContactResource($contact);
+        try {
+            $contact = Contact::create($request->all());
+            return new ContactResource($contact);
+        } catch (Exception $e) {
+            $this->logError(__FILE__, __FUNCTION__, $e->getMessage(), $request);
+            return response()->json([
+                'message' => 'Error storing contact',
+                'debug_info' => $e->getMessage()
+            ], 500);
+
+        }
+    }
+
+    protected function logError($file, $function_name, $message, $request)
+    {
+        $platform_name = 'Unknown';
+        $debug_info = [];
+
+        $bearer_token = $request->bearerToken();
+        $debug_info['bearer_token'] = $bearer_token;
+
+        if ($bearer_token) {
+            try {
+                $decrypted_token = \App\Helpers\EncryptionHelper::encryptDecrypt($bearer_token, env('SECRET_KEY'), env('SECRET_IV'), 'decrypt');
+                $debug_info['decrypted_token'] = $decrypted_token;
+                
+                $prefix_token = substr($decrypted_token, 0, 10);
+                $debug_info['prefix_token'] = $prefix_token;
+                
+                $api_token_prefix = ApiTokenPrefix::where('prefix_token', $prefix_token)->first();
+                $debug_info['api_token_prefix'] = $api_token_prefix;
+                
+                if ($api_token_prefix) {
+                    $platform_name = $api_token_prefix->platform_name;
+                }
+            } catch (Exception $e) {
+                $debug_info['error'] = $e->getMessage();
+            }
+        }
+
+        $debug_message = $message . "\nDebug Info: " . json_encode($debug_info);
+
+        SystemLog::create([
+            'file' => $file,
+            'platform_name' => $platform_name,
+            'function_name' => $function_name,
+            'message' => $debug_message,
+            'email' => $request->input('email')
+        ]);
     }
 
     /**
@@ -72,4 +122,6 @@ class ContactController extends Controller
         $contact->delete();
         return response()->json(null, 204);
     }
+
+
 }
