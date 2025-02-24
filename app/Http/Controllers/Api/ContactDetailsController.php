d3c7ad5ac2b8bd224e4c9078602779de6a15297a
GGallery <antonio@ggallery.it>
2025-02-24 11:36:45 +0100

    add some comments to logError()

--- app/Http/Controllers/Api/ContactDetailsController.php
+++ app/Http/Controllers/Api/ContactDetailsController.php
@@ -32,6 +32,7 @@ class ContactDetailsController extends Controller
             $contactDetails = ContactDetails::create($request->all());
             return new ContactDetailsResource($contactDetails);
         } catch (Exception $e) {
+            // Chiama la funzione logError
             $this->logError(__FILE__, __FUNCTION__, $e->getMessage(), $request);
             return response()->json([
                 'message' => 'Error storing contact details',
@@ -41,27 +42,34 @@ class ContactDetailsController extends Controller
         }
     }
 
+    //Funzione custom per i log errors con l'identificazione della piattaforma
     protected function logError($file, $function_name, $message, $request)
     {
+        // Recupera platform_name da api_tokens_prefixes, se disponibile
         $platform_name = 'Unknown'; // Valore default
         $debug_info = [];
 
-        // Recupera platform_name da api_tokens_prefixes, se disponibile
+        // Prende il bearer token da request per identificazione della piattaforma
         $bearer_token = $request->bearerToken();
+        // Array delle informazioni di debug
         $debug_info['bearer_token'] = $bearer_token;
     
         if ($bearer_token) {
             try {
+                // Decritta il token
                 $decrypted_token = \App\Helpers\EncryptionHelper::encryptDecrypt($bearer_token, env('SECRET_KEY'), env('SECRET_IV'), 'decrypt');
                 $debug_info['decrypted_token'] = $decrypted_token;
                 
+                // Dal token estrae il prefisso
                 $prefix_token = substr($decrypted_token, 0, 10);
                 $debug_info['prefix_token'] = $prefix_token;
                 
+                // Con il prefisso estratto cerca il record corrispondente il db
                 $api_token_prefix = ApiTokenPrefix::where('prefix_token', $prefix_token)->first();
                 $debug_info['api_token_prefix'] = $api_token_prefix;
                 
                 if ($api_token_prefix) {
+                    // Se lo trova viene associato alla variabile
                     $platform_name = $api_token_prefix->platform_name;
                 }
             } catch (Exception $e) {
@@ -69,9 +77,10 @@ class ContactDetailsController extends Controller
             }
         }
 
-        // Aggiungi le informazioni di debug al messaggio
+        // Aggiunge le informazioni di debug al messaggio
         $debug_message = $message . "\nDebug Info: " . json_encode($debug_info);
     
+        // Crea un record nel db con le informazioni di log
         SystemLog::create([
             'file' => $file,
             'platform_name' => $platform_name,
