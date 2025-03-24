6bfa87188789a7e4c9f52b469c27c7488422a5b6
GGallery <antonio@ggallery.it>
2025-03-24 09:47:05 +0100

    commit test

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([\App\Http\Middleware\AuthenticateWithToken::class])->group(function () {
     Route::apiResource('contacts', ContactController::class);
     Route::apiResource('contacts_details', ContactDetailsController::class);
+    
 });
